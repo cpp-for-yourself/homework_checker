8021a8728c47041f8d9e20d5705c71ffcaecb925
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2020-11-16 00:31:21 +0100

    Fix linter

--- homework_checker/__init__.py
+++ homework_checker/__init__.py
@@ -2,11 +2,11 @@
 
 name = "homework_checker"
 
-__all__ = ("check_homework",
-           "checker",
-           "md_writer",
-           "schema_manager",
-           "schema_tags",
-           "tasks"
-           "tools"
-           "tests")
+__all__ = (
+    "check_homework",
+    "checker",
+    "md_writer",
+    "schema_manager",
+    "schema_tags",
+    "tasks" "tools" "tests",
+)
