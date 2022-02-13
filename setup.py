ce25cce5d4cb8d9e6c12c4781dfbd7c6632f0e09
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2022-02-13 19:06:57 +0100

    Change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils import log
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION_STRING = "1.0.1"
+VERSION_STRING = "1.0.2"
 
 PACKAGE_NAME = "homework_checker"
 
